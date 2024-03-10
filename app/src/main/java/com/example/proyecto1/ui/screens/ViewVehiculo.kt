f68ad10becc50414859c8ed0fe9090d80e5e1e9b
Aingeru <1405bellido@gmail.com>
2024-03-10 23:41:34 +0100

    Reogranized ViewVehiculo screen

--- app/src/main/java/com/example/proyecto1/ui/screens/ViewVehiculo.kt
+++ app/src/main/java/com/example/proyecto1/ui/screens/ViewVehiculo.kt
@@ -3,6 +3,7 @@ package com.example.proyecto1.ui.screens
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,6 +19,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -57,216 +59,214 @@ fun ViewVehiculo(
         viewModel.getVehicleServices(it).collectAsState(initial = emptyList()).value
     }
 
-    var deletingServicio by remember {
+    var deletingServicio = remember {
         mutableStateOf(
             Servicio("a", "a", "a")
         )
     }
 
-    var showDeleteAlertDialog by remember {
+    var showDeleteAlertDialog = remember {
         mutableStateOf(false)
     }
 
-    if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT) {
-        Scaffold (
-            topBar = {
-                TopBar(
-                    title = stringResource(id = R.string.Info_vehicle),
-                    showSettings = false,
-                    showBackNavArrow = true,
-                    navController = navController
-                )
-            }
-        ) { innerPadding ->
-            Column (
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .padding(all = 15.dp)
-            ) {
-                Row { Text(text = "Nombre cliente: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.nombreCliente) }
-                Row { Text(text = "Matrícula: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.matricula) }
-                Row { Text(text = "Marca: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.marca) }
-                Row { Text(text = "Modelo: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.modelo) }
-                Text(
-                    text = stringResource(id = R.string.Vehicle_services),
-                    fontSize = 20.sp,
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Center
-                )
-                LazyColumn {
-                    if (serviciosDelVehiculo != null) {
-                        for (servicio in serviciosDelVehiculo) {
-                            item {
-                                ElevatedCard (
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(6.dp)
-                                ) {
-                                    Column (
-                                        modifier = Modifier.padding(10.dp)
-                                    ) {
-                                        Row (
-                                            horizontalArrangement = Arrangement.Center,
-                                            verticalAlignment = Alignment.CenterVertically,
-                                            modifier = Modifier.fillMaxWidth()
-                                        ) {
-                                            Column {
-                                                Text(text = servicio.fecha)
-
-                                            }
-                                            Spacer(modifier = Modifier.weight(1f))
-                                            IconButton(onClick = {
-                                                val servicioParaEnviar = servicio.fecha.replace("/", "-")
-                                                navController.navigate("viewServicio/$servicioParaEnviar")
-                                            }) {
-                                                Icon(
-                                                    painterResource(id = R.drawable.baseline_remove_red_eye_24),
-                                                    contentDescription = "Ver"
-                                                )
-                                            }
-                                            IconButton(onClick = {
-                                                showDeleteAlertDialog = true
-                                                deletingServicio = servicio
-                                            }) {
-                                                Icon(
-                                                    imageVector = Icons.Rounded.Delete,
-                                                    contentDescription = "Eliminar",
-                                                )
-                                            }
-                                        }
-                                        Text(text = servicio.descripcion, maxLines = 1)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                if (showDeleteAlertDialog) {
-                    AlertDialog(
-                        title = { Text(text = "¿Estás seguro?") },
-                        text = { Text(text = "¿Estás seguro de que deseas eliminar el elemento seleccionado?") },
-                        confirmButton = {
-                            Button(onClick = {
-                                showDeleteAlertDialog = false
-                                viewModel.deleteServicio(deletingServicio)
-                            }) {
-                                Text(text = "Confirmar")
-                            }
-                        },
-                        dismissButton = {
-                            Button(onClick = { showDeleteAlertDialog = false }) {
-                                Text(text = "Cancelar")
-                            }
-                        },
-                        onDismissRequest = { showDeleteAlertDialog = false },
-                    )
-                }
-            }
+    Scaffold (
+        topBar = {
+            TopBar(
+                title = stringResource(id = R.string.Info_vehicle),
+                showSettings = false,
+                showBackNavArrow = true,
+                navController = navController
+            )
+        }
+    ) { innerPadding ->
+        if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            PortraitLayout(
+                innerPadding = innerPadding,
+                vehiculo = vehiculo,
+                serviciosDelVehiculo = serviciosDelVehiculo,
+                navController = navController,
+                showDeleteAlertDialog = showDeleteAlertDialog,
+                deletingServicio = deletingServicio
+            )
+        }
+        else {
+            LandscapeLayout(
+                innerPadding = innerPadding,
+                vehiculo = vehiculo,
+                serviciosDelVehiculo = serviciosDelVehiculo,
+                navController = navController,
+                showDeleteAlertDialog = showDeleteAlertDialog,
+                deletingServicio = deletingServicio
+            )
         }
     }
-    else {
-        Scaffold (
-            topBar = {
-                TopBar(
-                    title = stringResource(id = R.string.Info_vehicle),
-                    showSettings = false,
-                    showBackNavArrow = true,
-                    navController = navController
-                )
-            }
-        ) { innerPadding ->
-            Row (
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .padding(10.dp)
-            ) {
-                Column (
-                    modifier = Modifier.weight(1f)
-                ) {
-                    Row { Text(text = "Nombre cliente: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.nombreCliente) }
-                    Row { Text(text = "Matrícula: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.matricula) }
-                    Row { Text(text = "Marca: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.marca) }
-                    Row { Text(text = "Modelo: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.modelo) }
-                    if (showDeleteAlertDialog) {
-                        AlertDialog(
-                            title = { Text(text = "¿Estás seguro?") },
-                            text = { Text(text = "¿Estás seguro de que deseas eliminar el elemento seleccionado?") },
-                            confirmButton = {
-                                Button(onClick = {
-                                    showDeleteAlertDialog = false
-                                    viewModel.deleteServicio(deletingServicio)
+    if (showDeleteAlertDialog.value) {
+        DeleteAlertDialog(
+            showDeleteAlertDialog = showDeleteAlertDialog,
+            viewModel = viewModel,
+            deletingServicio = deletingServicio
+        )
+    }
+}
+
+@Composable
+fun PortraitLayout(
+    innerPadding: PaddingValues,
+    vehiculo: Vehiculo,
+    serviciosDelVehiculo: List<Servicio>?,
+    navController: NavController,
+    showDeleteAlertDialog: MutableState<Boolean>,
+    deletingServicio: MutableState<Servicio>
+) {
+    Column (
+        modifier = Modifier
+            .padding(innerPadding)
+            .padding(all = 15.dp)
+    ) {
+        VehicleInfo(vehiculo = vehiculo, modifier = Modifier)
+        Text(
+            text = stringResource(id = R.string.Vehicle_services),
+            fontSize = 20.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        VehicleServices(
+            serviciosDelVehiculo = serviciosDelVehiculo,
+            navController = navController,
+            showDeleteAlertDialog = showDeleteAlertDialog,
+            deletingServicio = deletingServicio
+        )
+    }
+}
+
+@Composable
+fun LandscapeLayout(
+    innerPadding: PaddingValues,
+    vehiculo: Vehiculo,
+    serviciosDelVehiculo: List<Servicio>?,
+    navController: NavController,
+    showDeleteAlertDialog: MutableState<Boolean>,
+    deletingServicio: MutableState<Servicio>
+) {
+    Row (
+        modifier = Modifier
+            .padding(innerPadding)
+            .padding(10.dp)
+    ) {
+        VehicleInfo(vehiculo = vehiculo, modifier = Modifier.weight(1f))
+        Column (
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = stringResource(id = R.string.Vehicle_services),
+                fontSize = 20.sp,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            VehicleServices(
+                serviciosDelVehiculo = serviciosDelVehiculo,
+                navController = navController,
+                showDeleteAlertDialog = showDeleteAlertDialog,
+                deletingServicio = deletingServicio
+            )
+        }
+    }
+}
+
+@Composable
+fun VehicleInfo(
+    vehiculo: Vehiculo,
+    modifier: Modifier
+) {
+    Column (
+        modifier = modifier
+    ) {
+        Row { Text(text = "Nombre cliente: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.nombreCliente) }
+        Row { Text(text = "Matrícula: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.matricula) }
+        Row { Text(text = "Marca: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.marca) }
+        Row { Text(text = "Modelo: ", fontWeight = FontWeight.Bold); Text(text = vehiculo.modelo) }
+    }
+}
+
+@Composable
+fun VehicleServices(
+    serviciosDelVehiculo: List<Servicio>?,
+    navController: NavController,
+    showDeleteAlertDialog: MutableState<Boolean>,
+    deletingServicio: MutableState<Servicio>
+) {
+    LazyColumn {
+        if (serviciosDelVehiculo != null) {
+            for (servicio in serviciosDelVehiculo) {
+                item {
+                    ElevatedCard (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(6.dp)
+                    ) {
+                        Column (
+                            modifier = Modifier.padding(10.dp)
+                        ) {
+                            Row (
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Column {
+                                    Text(text = servicio.fecha)
+
+                                }
+                                Spacer(modifier = Modifier.weight(1f))
+                                IconButton(onClick = {
+                                    val servicioParaEnviar = servicio.fecha.replace("/", "-")
+                                    navController.navigate("viewServicio/$servicioParaEnviar")
                                 }) {
-                                    Text(text = "Confirmar")
+                                    Icon(
+                                        painterResource(id = R.drawable.baseline_remove_red_eye_24),
+                                        contentDescription = "Ver"
+                                    )
                                 }
-                            },
-                            dismissButton = {
-                                Button(onClick = { showDeleteAlertDialog = false }) {
-                                    Text(text = "Cancelar")
-                                }
-                            },
-                            onDismissRequest = { showDeleteAlertDialog = false },
-                        )
-                    }
-                }
-                Column (
-                    modifier = Modifier.weight(1f)
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.Vehicle_services),
-                        fontSize = 20.sp,
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center
-                    )
-                    LazyColumn {
-                        if (serviciosDelVehiculo != null) {
-                            for (servicio in serviciosDelVehiculo) {
-                                item {
-                                    ElevatedCard (
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(6.dp)
-                                    ) {
-                                        Column (
-                                            modifier = Modifier.padding(10.dp)
-                                        ) {
-                                            Row (
-                                                horizontalArrangement = Arrangement.Center,
-                                                verticalAlignment = Alignment.CenterVertically,
-                                                modifier = Modifier.fillMaxWidth()
-                                            ) {
-                                                Column {
-                                                    Text(text = servicio.fecha)
-
-                                                }
-                                                Spacer(modifier = Modifier.weight(1f))
-                                                IconButton(onClick = {
-                                                    val servicioParaEnviar = servicio.fecha.replace("/", "-")
-                                                    navController.navigate("viewServicio/$servicioParaEnviar")
-                                                }) {
-                                                    Icon(
-                                                        painterResource(id = R.drawable.baseline_remove_red_eye_24),
-                                                        contentDescription = "Ver"
-                                                    )
-                                                }
-                                                IconButton(onClick = {
-                                                    showDeleteAlertDialog = true
-                                                    deletingServicio = servicio
-                                                }) {
-                                                    Icon(
-                                                        imageVector = Icons.Rounded.Delete,
-                                                        contentDescription = "Eliminar",
-                                                    )
-                                                }
-                                            }
-                                            Text(text = servicio.descripcion, maxLines = 1)
-                                        }
-                                    }
+                                IconButton(onClick = {
+                                    showDeleteAlertDialog.value = true
+                                    deletingServicio.value = servicio
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Rounded.Delete,
+                                        contentDescription = "Eliminar",
+                                    )
                                 }
                             }
+                            Text(text = servicio.descripcion, maxLines = 1)
                         }
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun DeleteAlertDialog(
+    showDeleteAlertDialog: MutableState<Boolean>,
+    viewModel: ActivityViewModel,
+    deletingServicio: MutableState<Servicio>
+) {
+    AlertDialog(
+        title = { Text(text = "¿Estás seguro?") },
+        text = { Text(text = "¿Estás seguro de que deseas eliminar el elemento seleccionado?") },
+        confirmButton = {
+            Button(onClick = {
+                showDeleteAlertDialog.value = false
+                viewModel.deleteServicio(deletingServicio.value)
+            }) {
+                Text(text = "Confirmar")
+            }
+        },
+        dismissButton = {
+            Button(onClick = { showDeleteAlertDialog.value = false }) {
+                Text(text = "Cancelar")
+            }
+        },
+        onDismissRequest = { showDeleteAlertDialog.value = false },
+    )
 }
